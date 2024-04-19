5564b1ea80200c8b920067d540a95f065440fb51
rkpatel001 <124277544+rkpatel001@users.noreply.github.com>
2024-04-19 14:57:12 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,124 @@
+
+
+
+
+let clientX = -100;
+let clientY = -100;
+const innerCursor = document.querySelector(".cursor--small");
+
+const initCursor = () => {
+
+  document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  });
+  
+  
+  const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+
+initCursor();
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCursor;
+
+const initCanvas = () => {
+  const canvas = document.querySelector(".cursor--canvas");
+  const shapeBounds = {
+    width: 50,
+    height: 50
+  };
+  paper.setup(canvas);
+  const strokeColor = "#ffb400";
+  const strokeWidth = 2;
+  const segments = 8;
+  const radius = 25;
+  
+  const noiseScale = 150; // speed
+  const noiseRange = 4; // range of distortion
+  let isNoisy = false; // state
+  
+  const polygon = new paper.Path.RegularPolygon(
+    new paper.Point(0, 0),
+    segments,
+    radius
+  );
+  polygon.strokeColor = strokeColor;
+  polygon.strokeWidth = strokeWidth;
+  polygon.smooth();
+  group = new paper.Group([polygon]);
+  group.applyMatrix = false;
+  
+  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  let bigCoordinates = [];
+  
+  
+  const lerp = (a, b, n) => {
+    return (1 - n) * a + n * b;
+  };
+  const map = (value, in_min, in_max, out_min, out_max) => {
+    return (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+  };
+  
+  paper.view.onFrame = event => {
+   
+    lastX = lerp(lastX, clientX, 0.2);
+    lastY = lerp(lastY, clientY, 0.2);
+    group.position = new paper.Point(lastX, lastY);
+  }
+}
+
+initCanvas();
+const initHovers = () => {
+
+  const handleMouseEnter = e => {
+    const navItem = e.currentTarget;
+    const navItemBox = navItem.getBoundingClientRect();
+    stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
+    stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
+    isStuck = true;
+  };
+  
+  const handleMouseLeave = () => {
+    isStuck = false;
+  };
+  
+  //Event listeners
+  const linkItems = document.querySelectorAll(".link");
+  linkItems.forEach(item => {
+    item.addEventListener("mouseenter", handleMouseEnter);
+    item.addEventListener("mouseleave", handleMouseLeave);
+  });
+};
+
+initHovers();
+
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
+
+
+
